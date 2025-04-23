5986e287009c883ba9e7e24d7656eb392a93e2c2
ac <ak.at.dawray@gmail.com>
2025-04-23 15:09:16 -0500

    moving and rotating triangle on screen

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
-pygame.init()
+from player import Player
 
-print("Starting Asteroids!")
-print(f"Screen width: {SCREEN_WIDTH}")
-print(f"Screen height: {SCREEN_HEIGHT}")
-
-#if __name__ == "__main__":
-#    main()
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-                
-        screen.fill("black")  # This is how you fill the screen
+
+        player.update(dt)
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
-main()
+
+if __name__ == "__main__":
+    main()
